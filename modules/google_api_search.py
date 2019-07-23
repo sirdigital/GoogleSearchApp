be6523acdc93267f2c3e15e489b6899b1bb4707c
sirdigital <dawidbodych@gmail.com>
2019-07-23 10:57:16 +0200

    catching http error

--- modules/google_api_search.py
+++ modules/google_api_search.py
@@ -3,14 +3,15 @@ from config import google_config
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError as GoogleHttpError
+import httplib2
 
 
 def google_search(search_term, **kwargs):
-    service = build("customsearch", "v1", developerKey=google_config['api_key'])
     try:
+        service = build("customsearch", "v1", developerKey=google_config['api_key'])
         res = service.cse().list(q=search_term, cx=google_config['cse_id'], **kwargs).execute()
         return res
-    except GoogleHttpError:
+    except (GoogleHttpError, httplib2.ServerNotFoundError):
         return None
 
 
